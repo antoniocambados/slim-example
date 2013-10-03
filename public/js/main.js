95f703465d287c2897c7a18e668db72fe619a071
Antonio Cambados <toniocambados@gmail.com>
2013-10-03 23:36:40 +0200

    Full-height sections
    Now sections resize to full height of the viewport (or more if the its content needs so).

---
+++ public/js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	// Set full-height sections
+	function set_full_height(selector) {
+		$(window).resize(function() {
+			$(selector).each(function(index) {
+				window_height   = $(window).height();
+				selector_height = $(this).height();
+				$(this).height( (selector_height > window_height) ? selector_height : window_height );
+			});
+		});
+		$(window).resize();
+	};
+
+	set_full_height(".full-height");
+});
