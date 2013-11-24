e4da752cc4a57fb03e9e5f95988d7ae97a70cc28
Antonio Cambados <toniocambados@gmail.com>
2013-11-24 22:09:55 +0100

    Cummulative seeding execution tool

---
+++ app/database/seed.php
@@ -0,0 +1,15 @@
+<?php
+
+$seedsDirectory = __DIR__.'/seeds';
+
+// Seed files, in order of execution
+$seeds = array(
+	'profiles',
+);
+
+// Execute all the migrations
+foreach ($seeds as $seed) {
+	echo "Executing seed \"$seed\"...";
+	require $seedsDirectory.'/'.$seed.'.php';
+	echo " Done.\n";
+}
