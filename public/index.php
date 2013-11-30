5488ae406f577d85a848ecf898110452d1f876e0
Antonio Cambados <toniocambados@gmail.com>
2013-11-30 21:09:57 +0100

    Categories and projects have their own routes and views.

--- public/index.php
+++ public/index.php
@@ -42,6 +42,7 @@ $app->view->parserExtensions = array(new \Slim\Views\TwigExtension());
 
 
 // Define routes
+// - Index
 $app->get('/', function () use ($app) {
     // Get all the objects needed for the view
     $parameters = array(
@@ -50,7 +51,28 @@ $app->get('/', function () use ($app) {
     );
 
     // Render view
-  $app->render('index.html.twig', $parameters);
+    $app->render('index.html.twig', $parameters);
+});
+// - Category
+$app->get('/category/:category_permalink', function ($category_permalink) use ($app) {
+    // Get all the objects needed for the view
+    $parameters = array(
+        'category' => Category::with('projects')->where('permalink', '=', $category_permalink)->first(),
+    );
+
+    // Render view
+    $app->render('category.html.twig', $parameters);
+});
+// - Project
+$app->get('/category/:category_permalink/:project_permalink', function ($category_permalink, $project_permalink) use ($app) {
+    // Get all the objects needed for the view
+    $category = Category::where('permalink', '=', $category_permalink)->first();
+    $parameters = array(
+        'project' => Project::where('permalink', '=', $project_permalink)->where('category_id', '=', $category->id)->first(),
+    );
+
+    // Render view
+    $app->render('project.html.twig', $parameters);
 });
 
 // 404 Not Found handler
