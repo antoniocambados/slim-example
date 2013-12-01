8b77776c3ece4c5e8b08f5bb024cd137069beb24
Antonio Cambados <toniocambados@gmail.com>
2013-12-01 17:32:43 +0100

    Named routes
    Defined named routes, and using them for generating links in the views.

--- app/routes/routes.php
+++ app/routes/routes.php
@@ -9,16 +9,16 @@
 $app->get('/', function () use ($app) {
      $controller = new Portfolio\Controller\IndexController($app);
      $controller->index();
-});
+})->name('index');
 
 // Category index
 $app->get('/category/:category', function ($category) use ($app) {
     $controller = new Portfolio\Controller\CategoryController($app);
     $controller->index($category);
-});
+})->name('category');
 
 // Project
 $app->get('/category/:category/:project', function ($category, $project) use ($app) {
     $controller = new Portfolio\Controller\CategoryController($app);
     $controller->project($category, $project); 
-});
+})->name('project');
